66194919f57596391472179a20c8878fb149ee65
J <hieutk13.4@gmail.com>
2020-04-10 01:12:20 +0700

    create admin and dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin','AdminController@index')->name('admin-index');
+Route::get('/dash-board','DashboardController@index')->name('admin-home');
